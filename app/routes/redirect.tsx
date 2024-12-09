86d5f438fbb9ebfea1d2d0c5921464c8b3305632
Thomas Welton <thomaswelton@me.com>
2024-12-09 12:11:44 +0000

    Remove randomisation

--- app/routes/redirect.tsx
+++ app/routes/redirect.tsx
@@ -1,11 +1,5 @@
 import { redirect } from '@remix-run/react';
 
 export const loader = () => {
-    const random = Math.random();
-    if (random < 0.5) {
-        console.log('issuing redirect to /');
-        return redirect('/');
-    }
-
-    return null;
+    return redirect('/');
 };
